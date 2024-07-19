7d87235ed025e5424a024a5a9554b5f0b4f2cc9b
deyniloy7 <dey.niloy007@gmail.com>
2024-07-20 03:35:13 +0530

    Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CommonModule } from '@angular/common';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule, MatSidenavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements AfterViewInit {
+  title = 'Infrrd';
+  @ViewChild('sidenav', { static: true })
+  public sidenav!: MatSidenav;
+  ngAfterViewInit(): void {
+    this.sidenav.open();
+  }
 }
